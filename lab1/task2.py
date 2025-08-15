cb28ffe3c05d8a377494a048867e1fe64569b151
maphy9 <maphy9@gmail.com>
2025-08-15 21:26:28 +0200

    lab1/task2

---
+++ lab1/task2.py
@@ -0,0 +1,23 @@
+from task1 import neuron
+
+def neural_network(input, layer_weights, biases=None, activation_function=lambda x: x):
+    neuron_count = len(layer_weights)
+    if biases is None:
+        biases = [0] * neuron_count
+    output = []
+    for weights, bias in zip(layer_weights, biases):
+        output.append(neuron(input, weights, bias, activation_function))
+    return output
+
+
+if __name__ == '__main__':
+    input = [0.5, 0.75, 0.1]
+    layer_weights = [
+        [0.1, 0.1, -0.3],
+        [0.1, 0.2, 0.0],
+        [0.0, 0.7, 0.1],
+        [0.2, 0.4, 0.0],
+        [-0.3, 0.5, 0.1]
+    ]
+    
+    print(neural_network(input, layer_weights))
